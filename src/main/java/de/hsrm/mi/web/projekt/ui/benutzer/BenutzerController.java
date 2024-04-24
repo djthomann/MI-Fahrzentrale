8be6071b54a82c7032255edd81023e2fb05ecc8d
David Thomann <david.thomann@student.hs-rm.de>
2024-04-24 14:05:07 +0200

    Started Adding BenutzerFormular to SessionScope

--- src/main/java/de/hsrm/mi/web/projekt/ui/benutzer/BenutzerController.java
+++ src/main/java/de/hsrm/mi/web/projekt/ui/benutzer/BenutzerController.java
@@ -5,22 +5,53 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
 
 
 @Controller
+@SessionAttributes(names = {"formular"})
 public class BenutzerController {
     
     Logger logger = LoggerFactory.getLogger(BenutzerController.class);
 
+    @ModelAttribute("formular")
+    public void initForm(Model m) {
+        m.addAttribute("formular", new BenutzerFormular());
+    }
+
     @GetMapping("/benutzer/{bnummer}")
-    public String getBenutzer(@PathVariable("bnummer") long bnummer) {
+    public String getBenutzer(@PathVariable("bnummer") long bnummer, Model m) {
         
         logger.info("bnummer = {}", bnummer);
         
-        // m.addAttribute("bnummer", bnummer);
+        m.addAttribute("bnummer", bnummer);
+        m.addAttribute("formular", new BenutzerFormular());
         return "benutzerbearbeiten";
     }
+
+    @PostMapping("/benutzer/{bnummer}")
+    public String postMethodName(
+    @RequestParam("name") String name,
+    @RequestParam("mail") String mail,
+    @PathVariable("bnummer") long bnummer, 
+    @ModelAttribute("formular") BenutzerFormular formular, 
+    Model m) {
+        
+        formular.setMail(mail);
+        formular.setName(name);
+
+        logger.info("mail = {}", mail);
+        logger.info("name = {}", name);
+        
+        return "benutzerbearbeiten";
+    }
+    
     
 
 }
