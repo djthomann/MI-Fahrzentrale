14658e13e07c7c0490f1f27cc608d0f66e53d074
David Thomann <david.thomann@student.hs-rm.de>
2024-04-24 14:04:47 +0200

    Added BenutzerFormular

---
+++ src/main/java/de/hsrm/mi/web/projekt/ui/benutzer/BenutzerFormular.java
@@ -0,0 +1,28 @@
+package de.hsrm.mi.web.projekt.ui.benutzer;
+
+public class BenutzerFormular {
+    
+    private String name;
+    private String mail;
+    private String birthday;
+    
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getMail() {
+        return mail;
+    }
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+    public String getBirthday() {
+        return birthday;
+    }
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+}
