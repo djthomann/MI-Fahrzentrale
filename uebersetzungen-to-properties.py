a3b2350c3b16ac0f287cedb7ed442c99a42697c7
David Thomann <david.thomann@student.hs-rm.de>
2024-05-10 22:06:41 +0200

    Added Internationalisation/Localisation

--- uebersetzungen-to-properties.py
+++ uebersetzungen-to-properties.py
@@ -9,7 +9,7 @@ firstLine = csv.readline()
 
 # print(firstLine)
 
-languages = firstLine.strip().split(";")
+languages = firstLine.strip().split(",")
 languages.pop(0)
 # print(languages)
 
@@ -22,24 +22,31 @@ for language in languages:
     messages[language] = []
 
 for line in lines:
-    if line.startswith("#"):
+    if line.startswith("#") or line.startswith(","):
         continue
     line = line.strip()
     if line:
-        parts = line.split(";")
+        parts = line.split(",")
         for count, language in enumerate(languages):
             messages[language].append((parts[0], parts[count + 1]))
 
 # print(messages["de"])
 
-for language in languages:
-    f = open(filepath + "messages_{}.properties".format(language), "x")
-    pairs = messages[language]
-    for i, pair in enumerate(pairs):
-        if i == len(pairs)- 1:  
-            f.write(pair[0] + "=" + pair[1])  
-        else:
+for i, language in enumerate(languages):
+    if i == 0:
+        f1 = open(filepath + "messages_{}.properties".format(language), "x")
+        f2 = open(filepath + "messages.properties", "x")
+        pairs = messages[language]
+        for pair in pairs:
+            f1.write(pair[0] + "=" + pair[1] + "\n")  
+            f2.write(pair[0] + "=" + pair[1] + "\n")  
+        f1.close()
+        f2.close()
+    else: 
+        f = open(filepath + "messages_{}.properties".format(language), "x")
+        pairs = messages[language]
+        for pair in pairs:
             f.write(pair[0] + "=" + pair[1] + "\n")  
-    
-    f.close()
+        
+        f.close()
     
