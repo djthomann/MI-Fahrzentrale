f48ca679fb7b22ae281f3c6a91e21763b490e554
David Thomann <david.thomann@student.hs-rm.de>
2024-05-15 13:17:05 +0200

    Added BenutzerService and its Implementation

---
+++ src/main/java/de/hsrm/mi/web/projekt/services/benutzer/BenutzerServiceImpl.java
@@ -0,0 +1,67 @@
+package de.hsrm.mi.web.projekt.services.benutzer;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+
+import de.hsrm.mi.web.projekt.entities.benutzer.Benutzer;
+import de.hsrm.mi.web.projekt.entities.benutzer.BenutzerRepository;
+
+public class BenutzerServiceImpl implements BenutzerService {
+
+    @Autowired private BenutzerRepository benutzerRepository;
+
+    Logger logger = LoggerFactory.getLogger(BenutzerServiceImpl.class);
+
+    @Override
+    public List<Benutzer> holeAlleBenutzer() {
+        List<Benutzer> benutzer =  benutzerRepository.findAll(Sort.by("name"));
+
+        if(benutzer.isEmpty()) {
+            logger.info("No Benutzer found, Database is empty");
+        } else {
+            logger.info("Retrieved Benutzer from Database" + benutzer.toString());
+        }
+
+        return benutzer;
+    }
+
+    @Override
+    public Optional<Benutzer> holeBenutzerMitId(long id) {
+
+        Optional<Benutzer> benutzer = benutzerRepository.findById(id);
+
+        if(benutzer.isEmpty()) {
+            logger.warn("Could not retrieve Benutzer with id: " + id);
+        } else {
+            logger.info("Retrieved Benutzer from database: " + benutzer.toString());
+        }
+
+        return benutzer;
+    }
+
+    @Override
+    public Benutzer speichereBenutzer(Benutzer b) {
+
+        logger.info("Saving Benutzer: " + b.toString());
+
+        Benutzer danach = benutzerRepository.save(b);
+
+        logger.info("Object after saving: " + danach.toString());
+
+        return danach;
+    }
+
+    @Override
+    public void loescheBenutzerMitId(long id) {
+
+        logger.info("Deleting Benutzer with id: " + id);
+
+        benutzerRepository.deleteById(id);
+    }
+    
+}
