185aef1e2cc8334e3bd8d4cb8558a09766b09c37
David Thomann <david.thomann@student.hs-rm.de>
2024-06-19 11:45:02 +0200

    Initialized Frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import TourenListe from '@/views/TourenListeView.vue'
+import { ref } from 'vue'
+
+const info = ref('Dies ist eine Nachricht')
+
+function infoAusblenden() {
+  info.value = ''
+}
+</script>
+
+<template>
+  <header>
+    <div>
+      <img th:src="@{/icons/Logo-Hochschule-RheinMain.png}" />
+      <a th:text="#{kopf.benutzer}">Benutzer bearbeiten</a>
+      <a>Orte</a>
+      <a>Touren</a>
+    </div>
+
+    <nav>
+      <a>DE</a>
+      <a>HE</a>
+      <a>BY</a>
+      <a>NL</a>
+      <a>EN</a>
+    </nav>
+  </header>
+
+  <span v-show="info !== ''" class="info-box"
+    >{{ info
+    }}<button class="info-button" v-on:click="infoAusblenden()" type="button">X</button></span
+  >
+
+  <TourenListeView></TourenListeView>
+
+  <footer>
+    <a th:href="@{~/kontakt}" th:text="#{fuss.kontakt}">Kontakt</a>
+    <a th:href="@{~/impressum}" th:text="#{fuss.impressum}">Impressum</a>
+  </footer>
+</template>
+
+<style scoped></style>
---
+++ frontend/src/components/tour/TourenListe.vue
@@ -0,0 +1,10 @@
+<template>
+  <span>Das ist eine Tour</span>
+</template>
+
+<script setup lang="ts">
+import ITourDTD from '@/components/tour/TourenListeView.vue'
+const props = defineProps({ tour: ITourDTD })
+</script>
+
+<style scoped></style>
