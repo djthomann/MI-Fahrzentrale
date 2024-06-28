850efa50f908f1f54c31969bcf983f9e17b067f8
David Thomann <david.thomann@student.hs-rm.de>
2024-06-29 01:25:29 +0200

    Added 300km Info String and fixed tour retrieval in TourView

--- frontend/src/views/TourView.vue
+++ frontend/src/views/TourView.vue
@@ -1,17 +1,28 @@
 <template>
-  <h1>Das ist mein Test</h1>
-  <h1>{{ tourid }}</h1>
   <h1>{{ tour }}</h1>
 </template>
 
 <script setup lang="ts">
 import { useTourenStore } from '@/stores/tourenstore'
+import { useInfo } from '@/composables/useInfo'
 const { tourdata, updateTourListe } = useTourenStore()
 updateTourListe()
+const { info, loescheInfo, setzeInfo } = useInfo()
+loescheInfo()
 
 const props = defineProps<{ tourid: string }>()
 
-const tour = tourdata.state.tourliste[parseInt(props.tourid)]
+const tour = tourdata.state.tourliste.find((value) => value.id === parseInt(props.tourid))
+
+if (tour) {
+  if (tour.distanz > 300) {
+    setzeInfo(
+      `Achtung! Die Tour von ${tour.startOrtName} nach ${tour.zielOrtName} ist länger als 300km`
+    )
+  }
+} else {
+  setzeInfo('Tour konnte nicht gefunden werden')
+}
 </script>
 
 <style scoped></style>
